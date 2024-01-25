6ff20c0c351933552adfff9b7ef7b5484ed26462
David Morales <davidmorales9611@gmail.com>
2024-01-24 22:39:58 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/brick_breacker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.brick_breacker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:brick_breacker/src/widgets/game_app.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GameApp());
+}
